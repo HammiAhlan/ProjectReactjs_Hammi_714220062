206dff8ac7a084ec97a74f065334644077a6681f
HammiAhlan <hammiahlan@gmail.com>
2022-12-14 19:35:00 +0700

    hasilreactjs

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App React Jss 
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Linus Tech Tips"
+          href="https://linustechtips.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
